a494e95a7ec4964faf6eb6931ef0c0f9d81d28e1
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-04-21 19:09:15 -0700

    add oncall/offcall

--- rtfd.js
+++ rtfd.js
@@ -1,21 +1,36 @@
 var fs = require('fs')
 var api = require('zenircbot-api');
+var Set = require('Set')
 var bot_config = api.load_config('../bot.json')
 var zen = new api.ZenIRCBot(bot_config.redis.host,
                             bot_config.redis.port,
                             bot_config.redis.db)
 var sub = zen.get_redis_client();
 var channels = ['#readthedocs']
+var oncall = new Set()
 
 fs.exists('./rtfd/rtfd.json', function (exists) {
     if (exists) {
         var conf = api.load_config('./rtfd/rtfd.json')
         channels = channels.concat(conf.channels)
+        if (conf.oncall) {
+            oncall = oncall.addAll(conf.oncall)
+        }
     }
 })
 
+var info = [
+    ('Welcome to #readthedocs, if you have a question please prefix it with ' +
+     '!help in order to notify the volunteer support people.'),
+    ('Please allow some time for a response as we may be asleep or ' +
+     'otherwise busy.'),
+    ("If you don't get a response please file an issue here: " +
+     "https://github.com/rtfd/readthedocs.org/issues")
+]
+
+
 zen.register_commands(
-    "rtd.js",
+    "rtfd.js",
     [
         {
             name: "oncall",
@@ -35,31 +50,25 @@ zen.register_commands(
 
 sub.subscribe('in');
 sub.on('message', function(channel, message){
-    var msg = JSON.parse(message);
-    console.log([msg.version == 1, msg.type == 'directed_privmsg',
-                channels.indexOf(msg.data.channel) != -1])
-    console.log(msg.type)
+    var msg = JSON.parse(message)
     if (msg.version == 1 && msg.type == 'directed_privmsg' &&
         channels.indexOf(msg.data.channel) != -1) {
         var help = /^help(\s.*)?$/.exec(msg.data.message)
-        if (/^info$/.test(msg.data.message.trim())) {
-            zen.send_privmsg(msg.data.channel,
-                             'Welcome to #readthedocs, if you have a ' +
-                             'question please prefix it with !help in order ' +
-                             'to notify the volunteer support people.')
-            zen.send_privmsg(msg.data.channel,
-                             'Please allow some time for a response as we ' +
-                             'may be asleep or otherwise busy.')
-            zen.send_privmsg(msg.data.channel,
-                             "If you don't get a response please file an " +
-                             "issue here: " +
-                             "https://github.com/rtfd/readthedocs.org/issues")
+        if (/^info$/.test(msg.data.message)) {
+            info.forEach(function (line) {
+                zen.send_privmsg(msg.data.channel, line)
+            })
         } else if (help) {
-            zen.send_privmsg(msg.data.channel, "Wraithan: ^")
-            var question = help[1].trim()
+            zen.send_privmsg(msg.data.channel,
+                             oncall.toArray().join(', ') + ': ^')
+            var question = help[1]
             if (question) {
-                fs.appendFile('./question.log', question + '\n')
+                fs.appendFile('./question.log', question.trim() + '\n')
             }
+        } else if (/^oncall$/.test(msg.data.message)) {
+            oncall.add(msg.data.sender)
+        } else if (/^offcall$/) {
+            oncall.remove(msg.data.sender)
         }
     }
 })
